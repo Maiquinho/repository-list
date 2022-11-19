78ad7829c02ef076c489837496a0b68de90c0e4f
Michael Matheus <michaelmatheus2001@gmail.com>
2022-11-19 14:15:20 -0300

    feat: aside component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Main } from "./components/Main";
 
 export function App() {
   return (
-    <div className="min-h-screen w-screen py-5 px-10 gap-x-16 gap-y-5 bg-purple-900 flex flex-wrap">
+    <div className="h-screen w-screen py-5 px-10 gap-x-16 gap-y-5 bg-purple-900 flex flex-wrap overflow-hidden">
 
       <Aside />
       <Main />
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { AsideCard } from "./Card";
-import { MapPin } from "phosphor-react";
+import { GithubLogo, LinkedinLogo, MapPin, TwitterLogo } from "phosphor-react";
 
 
 export function Aside() {
-    const [user, setUser] = useState({ name: '', avatar: '', bio: '', location: '' });
+    const [user, setUser] = useState({ name: '', avatar: '', bio: '', location: '', social: { twitter: '', linkedIn: '', gitHub: '' } });
 
 
     useEffect(() => {
@@ -16,7 +16,12 @@ export function Aside() {
                 name: data.name,
                 avatar: data.avatar_url,
                 bio: data.bio,
-                location: data.location
+                location: data.location,
+                social: {
+                    twitter: data.twitter_username,
+                    linkedIn: '',
+                    gitHub: data.html_url
+                }
             });
         }
 
@@ -25,7 +30,7 @@ export function Aside() {
 
 
     return (
-        <aside className="w-[348px] h-[90vh] text-purple-500 flex flex-col gap-7">
+        <aside className="w-[348px] h-[90vh] text-purple-500 flex flex-col gap-7 overflow-y-scroll scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent">
 
 
             <AsideCard className="flex flex-col justify-center items-center">
@@ -40,10 +45,22 @@ export function Aside() {
 
             <AsideCard>
 
-                <ul>
+                <ul className="flex flex-col gap-y-5">
                     <li className="text-sm flex items-center gap-x-5">
                         <MapPin width={24} height={24} />
                         {user.location}
+                    </li>
+                    <li className="text-sm flex items-center gap-x-5">
+                        <LinkedinLogo width={24} height={24} />
+                        {user.social.linkedIn}
+                    </li>
+                    <li className="text-sm flex items-center gap-x-5">
+                        <TwitterLogo width={24} height={24} />
+                        {user.social.twitter}
+                    </li>
+                    <li className="text-sm flex items-center gap-x-5">
+                        <GithubLogo width={24} height={24} />
+                        {user.social.gitHub}
                     </li>
                 </ul>
 
